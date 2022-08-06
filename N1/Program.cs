bec35b1d3dacac72488e25546b2738e3032061c2
Алексей Викулин <vaa@megamade.ru>
2022-08-06 19:52:43 +0300

    Add project files.

---
+++ N1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1    // Разработать структуру для решения линейного уравнения 0=kx+b. Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры. Осуществить использование экземпляра в программе.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            Lineq lineq = new Lineq();
+            Console.WriteLine(lineq.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ N1/lineq.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1
+{
+    struct  Lineq
+    {
+        double a;
+        double k;
+
+        public Lineq (double a, double k)
+        {
+            this.a = a;
+            this.k = k;
+        }
+        public  string Root()
+        {   
+            if (a == 0 && k == 0)
+                return "x - любое число";
+            double x = -a / k;
+            return $"x = {x}";
+        }
+            
+    }
+}
